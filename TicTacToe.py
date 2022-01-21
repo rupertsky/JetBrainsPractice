c68c051d90cad0d3a79cb31cd54f246cde43785d
rupertsky <cajas25@hotmail.com>
2022-01-20 19:13:56 -0500

    TicTacToe

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,108 +1,55 @@
-"""# write your code here
-x = input()
-chars = list(x)
+grid = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]] #Result Grid
+grid_pos = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]] #Positional Grid
+grid_empty = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]] #Empty while Grid
+empty_pos = []
+player_turn = 1
+state = 0
 
-def imposible(chars):
-
-    count_x = chars.count('X')
-    count_o = chars.count('O')
-
-    if abs(count_x - count_o) >= 2:
-        return True
-    else:
-        return False
 
 def winner(chars):
-    counter_x = 0
-    counter_o = 0
-    totalC = []
 
-    if chars[0] is not None and chars[0] != "_" and chars[0] is chars[1] and chars[1] is chars[2]:
+    if chars[0] is not None and chars[0] != " " and chars[0] is chars[1] and chars[1] is chars[2]:
         if chars[0] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    if chars[0] is not None and chars[0] != "_" and chars[0] is chars[3] and chars[3] is chars[6]:
-        if chars[0] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    if chars[0] is not None and chars[0] != "_" and chars[0] is chars[4] and chars[4] is chars[8]:
-        if chars[0] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    if chars[1] is not None and chars[1] != "_" and chars[1] is chars[4] and chars[4] is chars[7]:
-        if chars[1] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    if chars[2] is not None and chars[2] != "_" and chars[2] is chars[4] and chars[4] is chars[6]:
-        if chars[2] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    if chars[2] is not None and chars[2] != "_" and chars[2] is chars[5] and chars[5] is chars[8]:
-        if chars[2] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    if chars[3] is not None and chars[3] != "_" and chars[3] is chars[4] and chars[4] is chars[5]:
-        if chars[3] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    if chars[6] is not None and chars[6] != "_" and chars[6] is chars[7] and chars[7] is chars[8]:
-        if chars[6] == 'X':
-            counter_x += 1
-        else:
-            counter_o += 1
-    totalC.append(counter_x)
-    totalC.append(counter_o)
-    if counter_x == 0 and counter_o == 0:
-        return None
-    else:
-        return totalC
-
-def totalWins(o_x):
-    xWins = 0
-    oWins = 0
-    if o_x is not None:
-        if o_x[0] == 1:
-            xWins += 1
-        if o_x[1] == 1:
-            oWins += 1
-        if xWins == oWins:
             return True
-        else:
+        elif chars[0] == 'O':
+            return False
+    if chars[0] is not None and chars[0] != " " and chars[0] is chars[3] and chars[3] is chars[6]:
+        if chars[0] == 'X':
+            return True
+        elif chars[0] == 'O':
+            return False
+    if chars[0] is not None and chars[0] != " " and chars[0] is chars[4] and chars[4] is chars[8]:
+        if chars[0] == 'X':
+            return True
+        elif chars[0] == 'O':
+            return False
+    if chars[1] is not None and chars[1] != " " and chars[1] is chars[4] and chars[4] is chars[7]:
+        if chars[1] == 'X':
+            return True
+        elif chars[1] == 'O':
+            return False
+    if chars[2] is not None and chars[2] != " " and chars[2] is chars[4] and chars[4] is chars[6]:
+        if chars[2] == 'X':
+            return True
+        elif chars[2] == 'O':
+            return False
+    if chars[2] is not None and chars[2] != " " and chars[2] is chars[5] and chars[5] is chars[8]:
+        if chars[2] == 'X':
+            return True
+        elif chars[2] == 'O':
+            return False
+    if chars[3] is not None and chars[3] != " " and chars[3] is chars[4] and chars[4] is chars[5]:
+        if chars[3] == 'X':
+            return True
+        elif chars[3] == 'O':
+            return False
+    if chars[6] is not None and chars[6] != " " and chars[6] is chars[7] and chars[7] is chars[8]:
+        if chars[6] == 'X':
+            return True
+        elif chars[6] == 'O':
             return False
     else:
-        return False
-
-print(f---------
-#| {chars[0]} {chars[1]} {chars[2]} |
-#| {chars[3]} {chars[4]} {chars[5]} |
-#| {chars[6]} {chars[7]} {chars[8]} |
-#---------)
-
-if imposible(chars) or totalWins(winner(chars)):
-    print("Impossible")
-elif winner(chars) is not None:
-    if winner(chars)[0] == 1:
-        print("X wins")
-    else:
-        print("O wins")
-elif winner(chars) is None and imposible(chars) is False and chars.count('_') == 0 and chars.count(None) == 0:
-    print("Draw")
-elif winner(chars) is None and imposible(chars) is False:
-    print("Game not finished")
-else:
-    print("todo mal")
-"""
-X_O = input("Enter cells: ")
-grid_slots = list(X_O)
-grid_coordinates = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]
-confirm_coordinates = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]
+        return None
 
 
 def confirm_space(pos1, pos2, cor_list):
@@ -111,56 +58,74 @@ def confirm_space(pos1, pos2, cor_list):
             return cor_list.index(g)
 
 
-def new_grid(v_pos, coordinates, initial_slots):
-    for i in initial_slots:
-        for j in v_pos:
-            if i == 'X' and i == initial_slots[j]:
-                coordinates[j] = 'X'
-            elif i == 'O' and i == initial_slots[j]:
-                coordinates[j] = 'O'
-    return coordinates
-
-
-print(f"""---------
-| {grid_slots[0]} {grid_slots[1]} {grid_slots[2]} |
-| {grid_slots[3]} {grid_slots[4]} {grid_slots[5]} |
-| {grid_slots[6]} {grid_slots[7]} {grid_slots[8]} |
----------""")
-
-valid_pos = [val_pos for val_pos, val_char in enumerate(X_O) if val_char == 'X' or val_char == 'O']
-grid_coordinates = new_grid(valid_pos, grid_coordinates, grid_slots)
-empty_pos = []
-
-for h, y in enumerate(grid_coordinates):
-    if y != 'X':
-        if y != 'O':
+for h, o in enumerate(grid):
+    if o != 'X':
+        if o != 'O':
             empty_pos.append(h)
 
+
+for s in empty_pos:
+    if grid_empty[s] != 'X':
+        grid_empty[s] = ' '
+    elif grid_empty[s] != 'O' and grid_empty[s] != 'X':
+        grid_empty[s] = ' '
+
+
 while True:
+    print(f"""---------
+| {grid_empty[0]} {grid_empty[1]} {grid_empty[2]} |
+| {grid_empty[3]} {grid_empty[4]} {grid_empty[5]} |
+| {grid_empty[6]} {grid_empty[7]} {grid_empty[8]} |
+---------""")
     try:
         x, y = map(int, input("Enter the coordinates: ").split())
-        slot = confirm_space(x, y, confirm_coordinates)
+        slot = confirm_space(x, y, grid_pos)
         if 4 > x > 0 and 4 > y > 0:
-            if grid_coordinates[slot] == 'X' or grid_coordinates[slot] == 'O':
+            if grid[slot] == 'X' or grid[slot] == 'O':
                 print("This cell is occupied! Choose another one!")
             else:
-                for z in grid_coordinates:
-                    if z[0] == x and z[1] == y:
-                        pos = grid_coordinates.index(z)
-                        grid_coordinates[pos] = 'X'
+                if player_turn == 1:
+                    for z in grid:
+                        if z[0] == x and z[1] == y:
+                            pos = grid.index(z)
+                            grid[pos] = 'X'
+                            grid_empty[pos] = 'X'
+                            player_turn -= 1
+                elif player_turn == 0:
+                    for z in grid:
+                        if z[0] == x and z[1] == y:
+                            pos = grid.index(z)
+                            grid[pos] = 'O'
+                            grid_empty[pos] = 'O'
+                            player_turn += 1
+            if winner(grid):
+                state = 1
+                break
+            elif winner(grid) is False:
+                state = 2
+                break
+            elif winner(grid) is None and grid_empty.count(' ') == 0:
                 break
         else:
             print("Coordinates should be from 1 to 3!")
     except ValueError:
         print("You should enter numbers!")
-for y in empty_pos:
-    if grid_coordinates[y] != 'X':
-        grid_coordinates[y] = '_'
-    elif grid_coordinates[y] != 'O' and grid_coordinates[y] != 'X':
-        grid_coordinates[y] = '_'
+
+
+for f in empty_pos:
+    if grid[f] != 'X' and grid[f] != 'O':
+        grid[f] = ' '
+    elif grid[f] != 'O' and grid[f] != 'X':
+        grid[f] = ' '
 
 print(f"""---------
-| {grid_coordinates[0]} {grid_coordinates[1]} {grid_coordinates[2]} |
-| {grid_coordinates[3]} {grid_coordinates[4]} {grid_coordinates[5]} |
-| {grid_coordinates[6]} {grid_coordinates[7]} {grid_coordinates[8]} |
+| {grid[0]} {grid[1]} {grid[2]} |
+| {grid[3]} {grid[4]} {grid[5]} |
+| {grid[6]} {grid[7]} {grid[8]} |
 ---------""")
+if state == 1:
+    print("X wins")
+elif state == 2:
+    print("O wins")
+else:
+    print("Draw")
